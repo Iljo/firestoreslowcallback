6891cae40434fa8d4c49f62fb77bc0d288c200e4
Iljo <iljo.opposite@gmail.com>
2020-03-20 17:34:37 +0100

    Formatting date

--- app/src/main/java/com/example/firestoreslowcallback/MainActivity.kt
+++ app/src/main/java/com/example/firestoreslowcallback/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.firestoreslowcallback
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+const val LOG_TAG = "FirestoreIssue"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        datePickerView.init(2020, 3, 20) { _, year, month, dayOfMonth -> dateSelected(year, month, dayOfMonth) }
+        with(datePickerView) { dateSelected(year, month, dayOfMonth) }
+    }
+
+    private fun dateSelected(year: Int, month: Int, day: Int) {
+        val formattedDate = "$year-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}"
+        Log.d(LOG_TAG, formattedDate)
     }
 }
